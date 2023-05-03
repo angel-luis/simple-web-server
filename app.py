a375783510c0ac9429c526c6c2c2c0e6fcc82a53
angel-luis <angel.luis@me.com>
2023-05-03 14:06:20 +0200

    Add simple variable rule

--- app.py
+++ app.py
@@ -4,5 +4,10 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def homepage():
     return render_template("index.html")
+
+
+@app.route("/<name>")
+def homepage_with_name(name="Anonymous"):
+    return render_template("index.html", name=" " + name)
